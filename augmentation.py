320e4b00725d65b26a2231b12749c10b501e2854
ayslanleal <ayslanleal15@gmail.com>
2022-09-27 22:46:39 -0300

    augmentation

--- augmentation.py
+++ augmentation.py
@@ -3,6 +3,7 @@ import pandas as pd
 from pydub import AudioSegment
 import pydub
 
+
 def load_audio(path, duration):
     audio = pydub.AudioSegment.silent(duration=duration)
     audio = audio.overlay(AudioSegment.from_file(path).set_frame_rate(22050).set_channels(1))[0:duration]
@@ -11,8 +12,21 @@ def load_audio(path, duration):
 
 
 if __name__ == '__main__':
-    audio = AudioSegment.silent(duration=5000)
-    audio = audio.overlay(AudioSegment.from_file("./base.wav").set_frame_rate(22050).set_channels(1))[0:5000]
-    raw = (np.fromstring(audio._data, dtype="int16") + 0.5) / (0x7FFF + 0.5)
+    paths = ["./base.wav", "./base_2.wav"]
+    audios = []
+
+    for path in paths:
+        audio = AudioSegment.silent(duration=5000)
+        audio = audio.overlay(AudioSegment.from_file(path).set_frame_rate(22050).set_channels(1))[0:5000]
+        raw = (np.fromstring(audio._data, dtype="int16") + 0.5) / (0x7FFF + 0.5)
+        audios.append(raw)
+    
+    rows_audio = [np.vstack(audios)]
+    print(rows_audio[0])
+    
+
+
+
+
     
     
